1b8fcd7b52a7171ca5d8b39205cd204469f48854
Serhii <serhii.mask@gmail.com>
2023-09-16 06:16:16 +0300

    init: hw module 1 gradle

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person = new Person("Serhii", "Mykalo");
+
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(person));
+    }
+}
